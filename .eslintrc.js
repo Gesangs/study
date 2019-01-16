466d2a10ead05740938e4ea6174afdacd94533d5
Gesangs <865057951@qq.com>
2019-01-16 14:26:01 +0800

    fix

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": [
+    'standard',
+    'standard-jsx'
+  ],
+  'rules': {
+    'react/jsx-tag-spacing': ['error', {'beforeSelfClosing': 'never'}],
+    'camelcase': 0
+  }
+}
+  
